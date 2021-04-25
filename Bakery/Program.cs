d398f0406e428cbf84bd9441dd4edeb7c49ec3ba
Nick Hennessy <njhnny@gmail.com>
2021-04-25 01:18:13 -0700

    take order and return total price

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,6 +8,12 @@ namespace Bakery
     {
       Console.WriteLine("Welcome to Nick's bakery!");
       Console.WriteLine("Bread costs $2, Pastries cost $1");
+      Console.WriteLine("How many loaves of bread would you like?");
+      int breadNumber = int.Parse(Console.ReadLine());
+      Console.WriteLine("How many pastries would you like?");
+      int pastryNumber = int.Parse(Console.ReadLine());
+      int customerTotal = breadNumber + pastryNumber;
+      Console.WriteLine("Your total is: $" + customerTotal);
     }
   }
 }
