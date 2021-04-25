583f0bc44b2202193c2326f3406ff0d5c7f60a62
Nick Hennessy <njhnny@gmail.com>
2021-04-25 00:45:11 -0700

    create program to write welcome message and prices

---
+++ Bakery/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Bakery.Models;
+namespace Bakery
+{
+  public class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Welcome to Nick's bakery!");
+      Console.WriteLine("Bread costs $2, Pastries cost $1");
+    }
+  }
+}
