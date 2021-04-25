3070cff3892abeaf85910372f0ba41b5e86642db
Nick Hennessy <njhnny@gmail.com>
2021-04-25 00:13:39 -0700

    add boilerplate code, namespace and class

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BakeryTests
+  {
+    [TestMethod]
+    public void NameOfTestMethod()
+    {
+      //Arrange
+      //Act
+      //Assert
+    }
+  }
+}
