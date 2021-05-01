c012275c13eb2f32a6dba0e5624aa49bd14ba591
Nick Hennessy <njhnny@gmail.com>
2021-04-30 18:35:38 -0700

    separate pastry cost logic

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,6 +3,16 @@ namespace Bakery.Models
   public class Pastry
   {
     public int PastryCost = 2;
-    
+    public int PastryNumber { get; set; }
+    public Pastry(int pastryNumber)
+    {
+      PastryNumber = pastryNumber;
+    }
+    public int PastryCalculate()
+    {
+    int PastryTotal = PastryCost * pastryNumber;
+
+    return PastryTotal;
+    }
   }
 }
