ba14e0898c7a383284b662b1b2c13d7411ee423d
Nick Hennessy <njhnny@gmail.com>
2021-05-01 22:56:55 -0700

    correct pricing in ui

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,7 +7,8 @@ namespace Bakery
     static void Main()
     {
       Console.WriteLine("Welcome to Nick's bakery!");
-      Console.WriteLine("Bread costs $2, Pastries cost $1");
+      Console.WriteLine("Bread costs $5 per loaf. Buy two get one free!");
+      Console.WriteLine("Pastries are $2 each or 3 for $5");
       Console.WriteLine("How many loaves of bread would you like?");
       Bread BreadNumber = new Bread (int.Parse(Console.ReadLine()));
       Console.WriteLine("How many pastries would you like?");
