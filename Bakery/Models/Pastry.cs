e94961fc6e48a12fec852a1ce87a977d751c382b
Nick Hennessy <njhnny@gmail.com>
2021-05-01 18:27:33 -0700

    add to PastryCalculate and confirm previous tests pass

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,6 +3,7 @@ namespace Bakery.Models
   public class Pastry
   {
     public int PastryCost = 2;
+    public int ThreePastriesCost = 5;
     public int PastryNumber { get; set; }
     public Pastry(int pastryNumber)
     {
@@ -10,7 +11,10 @@ namespace Bakery.Models
     }
     public int PastryCalculate()
     {
-    int PastryTotal = PastryCost * PastryNumber;
+      int pastryRemainder = PastryNumber % 3;
+      int pastryTrios = (PastryNumber - pastryRemainder) / 3;
+    
+    int PastryTotal = (pastryTrios * ThreePastriesCost) + (pastryRemainder * PastryCost);
 
     return PastryTotal;
     }
